60b042f828e36671cba2c1eed2b741a3166401ed
Jonathan Vongchak <nexusbrainjr@gmail.com>
2025-01-25 11:36:17 +0100

    Revert "api documentation with swagger and redoc"
    This reverts commit f09884bf18f696a519ac69f509fe63a02d4714f6.

--- motaa/red.py
+++
@@ -1,8 +0,0 @@
-import redis
-
-r = redis.Redis(host='127.0.0.1', port=6379)
-try:
-    r.ping()
-    print("Connected to Redis!")
-except redis.ConnectionError as e:
-    print(f"Redis connection failed: {e}")
--- motaa/settings.py
+++ motaa/settings.py
@@ -120,14 +120,6 @@ else:
                     },
                 },
             ]
-    CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-            },
-        },
-    }
 
     WSGI_APPLICATION = 'motaa.wsgi.application'
 
