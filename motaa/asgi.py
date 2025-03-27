a5a91aafa6baf83786b8914876081cdc96f4d69a
Ogofe <7thogofe@gmail.com>
2025-03-27 17:11:22 +0100

    fix daphne server asgi reqs

--- motaa/asgi.py
+++ motaa/asgi.py
@@ -7,7 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
-import os
+import os, django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'motaa.settings')
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from chat.routing import urlpatterns
@@ -20,7 +24,6 @@ from chat.middleware import (
     ApiTokenAuthMiddleware,
 )
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'motaa.settings')
 
 # django's default application instance
 django_asgi_app = get_asgi_application()
