c7988e44bb943a5c909bc659a1006488580c9603
Dr milli <100302736+drmilli@users.noreply.github.com>
2025-10-19 02:51:30 +0100

    Update urls.py

--- veyu/urls.py
+++ veyu/urls.py
@@ -29,7 +29,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="MIT License"),
     ),
     public=True,
-    authentication_classes=[BasicAuthentication, SessionAuthentication],
+    authentication_classes=[],
     permission_classes=[permissions.AllowAny],
 )
 
