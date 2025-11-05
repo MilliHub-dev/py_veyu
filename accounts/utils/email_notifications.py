c62d4418c1d6cefbc2425070b5eca518d9cf0a01
Dr milli <100302736+drmilli@users.noreply.github.com>
2025-11-05 16:39:57 +0100

    Update email_notifications.py

--- accounts/utils/email_notifications.py
+++ accounts/utils/email_notifications.py
@@ -1,9 +1,13 @@
+import logging
 from typing import Optional, Dict, Any, List
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+
+# Set up logging
+logger = logging.getLogger(__name__)
 
 User = get_user_model()
 
