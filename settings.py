6f074a625e66c1480efa4e3e178fa5cbac434b75
Dr milli <100302736+drmilli@users.noreply.github.com>
2025-11-06 05:08:21 +0100

    h

--- settings.py
+++ settings.py
@@ -267,7 +267,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # EMAIL
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 465))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
